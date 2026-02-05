3deada36d5ab95ab9756b9e3386fa3dc0eee08a8
grace_chen <gyjchen@ucdavis.edu>
2026-02-04 22:31:31 -0800

    update

--- madlib/v2/script.js
+++ madlib/v2/script.js
@@ -7,16 +7,17 @@
     const outputView = document.querySelector('#output-view');
     const mantraPara = document.querySelector('#mantra');
 
-    form.addEventListener('submit', function(event){
-        event.preventDefault();
-
-        const place = document.querySelector('#place').value;
-        const item = document.querySelector('#item').value;
-        const combo = document.querySelector('#combo').value;
-        const animal = document.querySelector('#animal').value;
-        const adverb = document.querySelector('#adverb').value;
-   
     
+    form.addEventListener('submit', function(event){
+    event.preventDefault();
+      
+
+    const place = document.querySelector('#place').value;
+    const item = document.querySelector('#item').value;
+    const combo = document.querySelector('#combo').value;
+    const animal = document.querySelector('#animal').value;
+    const adverb = document.querySelector('#adverb').value;
+
     mantraPara.innerHTML = `<span>Yesterday</span> I walked to the <span>${place}</span>. It was then I discovered I had forgotten my <span>${item}</span>. I will have to  <span>${combo}</span> over the weekend. I went home and saw the <span>${animal}</span>. And I felt <span>${adverb}</span>`;
    
 
