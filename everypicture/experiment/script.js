ebe8b7d7c872841a572a094c9d15ba40f197365c
grace_chen <gyjchen@ucdavis.edu>
2026-02-19 19:03:29 -0800

    udate

--- everypicture/experiment/script.js
+++ everypicture/experiment/script.js
@@ -2,24 +2,45 @@
     'use strict';
     console.log('reading js');
 
-    document.addEventListener('mousemove', reportPos);
+    const shoulderOverlay= document.querySelector('#xrayOverlay');
+    const chestOverlay = document.querySelector('#xrayOverlayChest')
+
+    const area1 = document.querySelector('#area1');
+    const area2 = document.querySelector('#area2');
+
+
+    area1.addEventListener('mouseover', function() {
+        shoulderOverlay.style.opacity = "1";
+    });
+    area1.addEventListener('mouseout', function(){
+        shoulderOverlay.style.opacity = "0";
+    });
+
+    area2.addEventListener('mouseover', function(){
+        chestOverlay.style.opacity = "1";
+    });
+    area2.addEventListener('mouseout', function(){
+        chestOverlay.style.opacity = "0";
+    });
+
+    // document.addEventListener('mousemove', reportPos);
     
-    const theImg = document.querySelector('img');
-    let preLoc = 0;
+    // const theImg = document.querySelector('img');
+    // let preLoc = 0;
 
-    function reportPos(event) {
-        const windowSize = window.innerWidth;
-        const percent2px = windowSize / 4; 
-        const xPos = event.clientX;
-        const changePhoto = Math.floor(xPos / percent2px)
+    // function reportPos(event) {
+    //     const windowSize = window.innerWidth;
+    //     const percent2px = windowSize / 4; 
+    //     const xPos = event.clientX;
+    //     const changePhoto = Math.floor(xPos / percent2px)
 
-        if (changePhoto !==preLoc) {
-        theImg.src = `images/anatomy-0${changePhoto + 1}.png`;
-        preLoc = changePhoto;
-        console.log(preLoc);
-        }
+    //     if (changePhoto !==preLoc) {
+    //     theImg.src = `images/anatomy-0${changePhoto + 1}.png`;
+    //     preLoc = changePhoto;
+    //     console.log(preLoc);
+    //     }
 
-    }
+    // }
 
 
 })();
