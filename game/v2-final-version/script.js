048468aa7655fe307b0afa6d641a7c24b845b924
grace_chen <gyjchen@ucdavis.edu>
2026-03-11 23:22:33 -0700

    final

--- game/v2-final-version/script.js
+++ game/v2-final-version/script.js
@@ -1,136 +1,119 @@
 (function () {
-  "use strict";
-  console.log("reading js");
-  
-  
-  // 1. Data and Variables
-const gamePairs = [
-    { anatomy: "Anatomy 0", ID: "0" }, { img: "images/xray0.jpg", ID: "0" },
-    { anatomy: "Anatomy 1", ID: "1" }, { img: "images/xray1.jpg", ID: "1" },
-    { anatomy: "Anatomy 2", ID: "2" }, { img: "images/xray2.jpg", ID: "2" },
-    { anatomy: "Anatomy 3", ID: "3" }, { img: "images/xray3.jpg", ID: "3" },
-    { anatomy: "Anatomy 4", ID: "4" }, { img: "images/xray4.jpg", ID: "4" },
-    { anatomy: "Anatomy 5", ID: "5" }, { img: "images/xray5.jpg", ID: "5" }
-];
+    "use strict";
 
-const allCards = document.querySelectorAll('.card');
-const startBtn = document.querySelector('#start-btn');
-const pairsText = document.querySelector('h2');
+    console.log("reading js");
 
-let pairsFound = 0;
-let firstCard = null; 
-let secondCard = null;
-let isWaiting = false; // Prevents clicking during the flip-back delay
+    const cards = document.querySelectorAll(".card");
+    const startBtn = document.querySelector("#start-btn");
+    const overlay = document.querySelector("#overlay");
+    const gameOverText = document.querySelector("#gameOver");
+    const pairNum = document.querySelector("#pair-num");
 
-// 2. The missing function - This puts the data into your HTML
-function setupCards() {
-    for (let i = 0; i < allCards.length; i++) {
-        const cardBack = allCards[i].querySelector('.cardback');
-        const data = gamePairs[i];
+    const cardFlip = new Audio('sounds/flip.mp3'); 
+    const buttonClick = new Audio('sounds/ding.mp3');   
+    const cheer = new Audio('sounds/happy-sound.mp3');
 
-        // Give the card an ID so we can match it later
-        allCards[i].id = data.ID;
+    let firstCard = null;
+    let secondCard = null;
+    let matches = 0;
 
-        if (data.anatomy) {
-            cardBack.innerHTML = '<p class="cardtext">' + data.anatomy + '</p>';
-        } else if (data.img) {
-            cardBack.innerHTML = '<img src="' + data.img + '" alt="xray">';
+    const gameCards = [
+        { type: "text", content: "Shoulder", id: "0" },
+        { type: "img", content: "images/xray-shoulder.png", id: "0" },
+        { type: "text", content: "Chest", id: "1" },
+        { type: "img", content: "images/xray-chest.jpg", id: "1" },
+        { type: "text", content: "Foot", id: "2" },
+        { type: "img", content: "images/xray-foot.jpg", id: "2" },
+        { type: "text", content: "Hand", id: "3" },
+        { type: "img", content: "images/xray-hand.jpg", id: "3" },
+        { type: "text", content: "Neck", id: "4" },
+        { type: "img", content: "images/xray-neck.jpg", id: "4" },
+        { type: "text", content: "Skull", id: "5" },
+        { type: "img", content: "images/xray-skull.jpg", id: "5" }
+    ];
+
+    function shuffleCards() {
+        for (let i = gameCards.length - 1; i > 0; i--) {
+            let randomIndex = Math.floor(Math.random() * (i + 1));
+            let temp = gameCards[i];
+            gameCards[i] = gameCards[randomIndex];
+            gameCards[randomIndex] = temp;
         }
     }
-}
 
-// 3. Reset and Shuffle
-function startGame() {
-    startBtn.innerHTML = "Restart";
-    document.querySelector('#overlay').classList.add('hide');
-
-    pairsFound = 0;
-    pairsText.innerHTML = pairsFound;
-    firstCard = null;
-    secondCard = null;
-    isWaiting = false;
-    
-
-    for (let i = 0; i < gamePairs.length; i++) {
-        let randomNum = Math.floor(Math.random() * gamePairs.length);
-        let temporaryStorage = gamePairs[i];
-        gamePairs[i] = gamePairs[randomNum];
-        gamePairs[randomNum] = temporaryStorage;
-    }
-
-    for (let i = 0; i < allCards.length; i++) {
-        allCards[i].classList.remove("flip");
-    }
-    
-    setupCards(); 
-}
-
-// 4. Handle Clicks
-function clickCard(event) {
-
-    const clickedCard = event.currentTarget; 
-
-    if (isWaiting || clickedCard.classList.contains("flip") || clickedCard === firstCard) {
-        return;
-    }
-
-    clickedCard.classList.add("flip");
-
-    if (firstCard === null) {
-        firstCard = clickedCard;
-    } else {
-        secondCard = clickedCard;
-        checkMatch();
-    }
-}
-
-// 5. Check if IDs match (The simplified .id version)
-function checkMatch() {
-    const id1 = firstCard.id;
-    const id2 = secondCard.id;
-
-    if (id1 === id2) {
-        pairsFound = pairsFound + 1;
-        pairsText.innerHTML = pairsFound;
-        
-        if (pairsFound === 6) {
-            document.querySelector('#overlay').classList.remove('hide');
+    function setupCards() {
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].id = gameCards[i].id;
+            if (gameCards[i].type === "text") {
+                cards[i].querySelector(".cardback").innerHTML =
+                    `<p class="cardtext">${gameCards[i].content}</p>`;
+            } else {
+                cards[i].querySelector(".cardback").innerHTML =
+                    `<img src="${gameCards[i].content}" alt="xray">`;
+            }
         }
+    }
 
+    function resetTurn() {
         firstCard = null;
         secondCard = null;
-    } else {
-        isWaiting = true; 
-        setTimeout(function() {
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip");
-            firstCard = null;
-            secondCard = null;
-            isWaiting = false;
-        }, 500);
     }
-}
 
-// 6. Listeners
-startBtn.addEventListener('click', startGame);
+    function checkMatch() {
+        if (firstCard.id === secondCard.id) {
+            matches++;
+            pairNum.textContent = matches;
 
-for (let i = 0; i < allCards.length; i++) {
-    allCards[i].addEventListener('click', clickCard);
-}
+            if (matches === 6) {
+                cheer.play();
+                gameOverText.innerHTML = "Great job! You matched them all!";
+                overlay.classList.remove("hide");
+            }
 
-// Start the game for the first time
-setupCards();
+            resetTurn();
+        } else {
+            setTimeout(function () {
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+                resetTurn();
+            }, 500);
+        }
+    }
 
-const cheatBtn = document.querySelector('#cheat-code');
+    function flipCard() {
+        cardFlip.play()
+        if (firstCard && secondCard) return;
+        if (this === firstCard) return;
 
-cheatBtn.addEventListener('click', function() {
-    pairsFound = 6; 
-    pairsText.innerHTML = pairsFound; 
-    document.querySelector('#overlay').classList.remove('hide');// Show win screen
-    
-    
-    allCards.forEach(card => card.classList.add('flip'));
+        this.classList.add("flip");
 
-});
+        if (firstCard === null) {
+            firstCard = this;
+        } else {
+            secondCard = this;
+            checkMatch();
+        }
+    }
+
+    function startGame() {
+        buttonClick.play();
+        firstCard = null;
+        secondCard = null;
+        matches = 0;
+        pairNum.textContent = matches;
+        overlay.classList.add("hide");
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].classList.remove("flip");
+        }
+
+        shuffleCards();
+        setupCards();
+    }
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", flipCard);
+    }
+
+    startBtn.addEventListener("click", startGame);
 
 })();
